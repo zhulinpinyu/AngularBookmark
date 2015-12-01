24bd2cf8a309285512c93124cc54ef90f03c329c
zhulinpinyu <mulixiang199016@gmail.com>
2015-12-01 09:58:53 +0800

    basic gulp task learn

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,27 @@
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
+gulp.task("default",function(callback){
+  runSequence("build",callback);
+});
 
-gulp.task("default",function(){
-  console.log("Hello Gulp!");
+gulp.task("build",function(callback){
+  runSequence("clean","copy-build",callback);
+});
+
+gulp.task("clean", function(){
+  
+});
+
+gulp.task("copy-build",["copy-assets", "copy-app-js","copy-vendor-js"]);
+
+gulp.task("copy-assets",function(){
+
+});
+
+gulp.task("copy-app-js",function(){
+
+});
+
+gulp.task("copy-vendor-js",function(){
+
 });
